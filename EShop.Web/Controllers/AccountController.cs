fcf02fc9eabcea3613a5f7c8d432f35243b153e4
rzymski <83671265+rzymski@users.noreply.github.com>
2022-11-22 05:34:27 +0100

    Logowanie, Rejestracja, przypomnienie hasła.

---
+++ EShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using EShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Login = EShop.Web.Models.Login;
+
+namespace EShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                //TO DO
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+        public ActionResult Message()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                //TO DO
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(ForgotPassword forgotPassword)
+        {
+            if (ModelState.IsValid)
+            {
+                //TO DO
+                //return RedirectToAction("Message");
+                ViewBag.Msg = "Wysłaliśmy wiadomość na e-maila: " + (string)forgotPassword.Email;
+            }
+            else
+            {
+                ViewBag.Msg = "Dane nieprawidłowe";
+            }
+            return View();
+        }
+    }
+}
---
+++ EShop.Web/Models/Account.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EShop.Web.Models
+{
+    /*    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]*/
+    public class MustBeTrueAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return value != null && value is bool && (bool)value;
+        }
+    }
+
+    public class Registration
+    {
+        [DisplayName("Imię")]
+        public string Name { get; set; }
+
+        [DisplayName("Nazwisko")]
+        public string Surname { get; set; }
+
+        [Range(18, 100)]
+        [DisplayName("Aktualny Wiek*")]
+        public int Age { get; set; }
+
+        [DisplayName("Nazwa użytkownika*")]
+        [Required(ErrorMessage = "Pole nazwa użytkownika jest wymagane.")]
+        [StringLength(30, MinimumLength = 4)]
+        public string Username { get; set; }
+
+        [DisplayName("Hasło*")]
+        [StringLength(30, MinimumLength = 8)]
+        [Required(ErrorMessage = "Pole hasło jest wymagane.")]
+        public string Password { get; set; }
+
+        [DisplayName("Powtórz Hasło*")]
+        [Required(ErrorMessage = "Pole powtórz hasło jest wymagane.")]
+        [Compare("Password", ErrorMessage = "Powtórzone hasło musi być takie samo jak hasło.")]
+        public string ConfirmPassword { get; set; }
+
+        [DisplayName("E-mail*")]
+        [Required(ErrorMessage = "Pole email jest wymagane.")]
+        [RegularExpression(".+\\@.+\\..+$", ErrorMessage = "Email jest nieprawidłowy")]
+        public string Email { get; set; }
+
+        [DisplayName("Numer telefonu*")]
+        [Required(ErrorMessage = "Pole numer telefonu jest wymagane.")]
+        [RegularExpression(@"\d{9}$", ErrorMessage = "Numer telefonu nieprawidłowy.")]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("Zgoda formalna")]
+        [MustBeTrue(ErrorMessage = "Musisz zakceptować regulamin, żeby się zarejestrować.")]
+        public bool terms { get; set; }
+    }
+
+    public class Login
+    {
+        [DisplayName("Nazwa użytkownika")]
+        [Required(ErrorMessage = "Podaj nazwę użytkownika.")]
+        [StringLength(30, MinimumLength = 4)]
+        public string Username { get; set; }
+
+        [DisplayName("Hasło")]
+        [StringLength(30, MinimumLength = 8)]
+        [Required(ErrorMessage = "Podaj hasło.")]
+        public string Password { get; set; }
+    }
+
+    public class ForgotPassword
+    {
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "Podaj  email.")]
+        [RegularExpression(".+\\@.+\\..+$", ErrorMessage = "Email jest nieprawidłowy")]
+        public string Email { get; set; }
+    }
+
+
+}
