042d5a5f0473ede4080da65b9135900abea00b68
Piotr Szuflicki <pszuflicki@lbpro.pl>
2022-11-22 12:25:39 +0100

    Add product and adress forms

---
+++ EShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace EShop.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult AddEdit()
+        {
+            return View();
+        }
+        
+        public ActionResult AddEditAddress()
+        {
+            return View("_AddressAddEdit");
+        }
+    }
+}
---
+++ EShop.Web/Scripts/ViewScripts/Shared/Layout.ts
@@ -0,0 +1,10 @@
+﻿class Layout {
+    public init = (): void => {
+       $('form-select').
+    }
+}
+
+const layout: Layout = new Layout();
+$(document).ready(() => {
+   layout.init(); 
+});
